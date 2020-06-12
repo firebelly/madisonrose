6dbcc8aec0ef548ab524fbd1ca489dee9d137b85
poopsplat <matt.m.soria@gmail.com>
2020-06-12 11:17:02 -0500

    Making adjustments to the site nav

--- assets/scripts/routes/common.js
+++ assets/scripts/routes/common.js
@@ -3,6 +3,7 @@
 import jQueryBridget from 'jquery-bridget';
 import Masonry from 'masonry-layout';
 import ImagesLoaded from 'imagesloaded';
+import Velocity from 'velocity-animate';
 
 import appState from '../util/appState';
 
@@ -30,6 +31,14 @@ export default {
     _initHoverPairs();
     _initMobileNav();
 
+    // Keyboard navigation and esc handlers
+    $document.keyup(function(e) {
+      // esc
+      if (e.keyCode === 27) {
+        _closeNav();
+      }
+    });
+
     function _initHoverPairs() {
       $(document).on('mouseenter', '[data-hover-pair]', function(e) {
         var hoverPair = $(this).attr('data-hover-pair');
@@ -54,11 +63,48 @@ export default {
 
     function _openNav() {
       $body.addClass('nav-open');
+      if (!$('.page-overlay').length) {
+        $body.append('<div class="page-overlay"></div>');
+      }
+
+      $('.page-overlay').velocity({
+        opacity: 1
+      }, {
+        display: 'block',
+        easing: 'ease-out',
+        duration: 350
+      });
+
+      $siteNav.velocity({
+        opacity: 1
+      }, {
+        display: 'block',
+        easing: 'ease-out',
+        duration: 350
+      });
+
       $siteNav.addClass('-active');
     }
 
     function _closeNav() {
       $body.removeClass('nav-open');
+
+      $siteNav.velocity({
+        opacity: 0
+      }, {
+        display: 'none',
+        easing: 'ease-out',
+        duration: 350
+      });
+
+      $('.page-overlay').velocity({
+        opacity: 0
+      }, {
+        display: 'none',
+        easing: 'ease-out',
+        duration: 350
+      });
+
       $siteNav.removeClass('-active');
     }
 
